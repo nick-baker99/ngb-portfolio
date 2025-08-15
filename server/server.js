8dfaddb6d704809252fb3a05da01bb993678d42d
Nick B <ngbaker99@gmail.com>
2025-08-15 13:13:30 +0100

    deploy config update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
+const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const nodemailer = require('nodemailer');
-const path = require("path");
 
 const app = express();
 
